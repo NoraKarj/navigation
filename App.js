85c9bf0360169637fcdbfc7d74180815ae8a5d6d
NoraKarj <t1kano00@students.oamk.fi>
2023-09-23 15:36:37 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack'
 import { StyleSheet, Text, View } from 'react-native';
+import SecondScreen from './SecondScreen';
+import HomeScreen from './HomeScreen';
+import React from 'react';
+import 'react-native-gesture-handler';
+
 
 export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+   <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+      name="Home"
+      component={HomeScreen}
+      option={{      
+          title: 'Home',
+          headerTitle: 'Home',
+
+      }}
+      />
+      <Stack.Screen
+      name='Second'
+      component={SecondScreen}
+      option={{
+        title: 'Second',
+        headerTitle: 'Second'
+      }}
+      />
+   </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
